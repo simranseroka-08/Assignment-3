7235291a8c894b3219bbd78a96e3d1a885937003
simranseroka-08 <ssingh30_be26@thapar.edu>
2026-09-08 10:34:49 +0530

    Q6_math_functions

---
+++ 6.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<math.h>
+#define pi 3.14
+int main() {
+    printf("sin(pi/2) = %f\n", sin(pi/2));
+    printf("cos(pi) = %f\n", cos(pi));
+    printf("log(10) = %f\n", log(10));
+    printf("2^3 = %f\n", pow(2,3));
+    printf("sqrt(16) = %f\n", sqrt(16));
+    return 0;
+}
