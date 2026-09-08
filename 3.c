50c24557eabcc936a6ae64ae34d1362695c9e30b
simranseroka-08 <ssingh30_be26@thapar.edu>
2026-09-08 10:32:05 +0530

    Q3_format_console_output

---
+++ 3.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main() {
+    printf("Helloo\b Earth\n");
+    printf("Earth is beautiful\n");
+    printf("It\'s Marvellous\n");
+    printf("Humans said\"Hi\"\n");
+    printf("ABCDEF\rXYZ\n");
+     printf("Name\t Marks\t Class");
+    return 0;
+}
