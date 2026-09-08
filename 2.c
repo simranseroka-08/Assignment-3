c93b2123416c5cd444ee1f06733b75a242ba449f
simranseroka-08 <ssingh30_be26@thapar.edu>
2026-09-08 10:31:10 +0530

    Q2_implement_assignment_operators

---
+++ 2.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main() {
+    int a=12, b=6;
+    a+=b;
+    printf("a=%d\n",a);
+
+    a-=b;
+    printf("a=%d\n",a);
+
+    a*=b;
+    printf("a=%d\n",a);
+
+    a/=b;
+    printf("a=%d\n",a);
+
+    return 0;
+}
